9bf28f57afa8d4b9f617332251ca247321298ced
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-05-29 16:50:43 +0300

    refactored init 3

--- main.tf
+++ main.tf
@@ -82,6 +82,17 @@ resource "azurerm_network_security_group" "cicd" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 80
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "cicd" {
