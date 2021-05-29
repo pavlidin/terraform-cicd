37954198d2827590935c77e52416b3d2822ad4e9
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-05-29 17:06:12 +0300

    refactored init 8

--- main.tf
+++ main.tf
@@ -72,13 +72,24 @@ resource "azurerm_network_security_group" "cicd" {
   location            = azurerm_resource_group.cicd.location
   resource_group_name = azurerm_resource_group.cicd.name
   security_rule {
-    name                       = "troll"
+    name                       = "SSH"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
