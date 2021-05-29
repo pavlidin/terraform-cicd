59f870a7e37d46a4eaac871396b0ed8aa9c50c26
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-05-29 17:47:15 +0300

    new era5

--- main.tf
+++ main.tf
@@ -16,52 +16,52 @@ provider "azurerm" {
 }
 
 # Create a resource group if it doesn't exist
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
+resource "azurerm_resource_group" "cicdgroup" {
+    name     = "mycicdgroup"
     location = "West Europe"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "CICD Infrastructure"
     }
 }
 
 # Create virtual network
-resource "azurerm_virtual_network" "myterraformnetwork" {
+resource "azurerm_virtual_network" "cicdnetwork" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
     location            = "West Europe"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
+    resource_group_name = azurerm_resource_group.cicdgroup.name
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "CICD Infrastructure"
     }
 }
 
 # Create subnet
-resource "azurerm_subnet" "myterraformsubnet" {
+resource "azurerm_subnet" "mycicdsubnet" {
     name                 = "mySubnet"
-    resource_group_name  = azurerm_resource_group.myterraformgroup.name
-    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    resource_group_name  = azurerm_resource_group.cicdgroup.name
+    virtual_network_name = azurerm_virtual_network.cicdnetwork.name
     address_prefixes       = ["10.0.1.0/24"]
 }
 
 # Create public IPs
-resource "azurerm_public_ip" "myterraformpublicip" {
+resource "azurerm_public_ip" "mycicdpublicip" {
     name                         = "myPublicIP"
     location                     = "West Europe"
-    resource_group_name          = azurerm_resource_group.myterraformgroup.name
+    resource_group_name          = azurerm_resource_group.cicdgroup.name
     allocation_method            = "Dynamic"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "CICD Infrastructure"
     }
 }
 
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "myterraformnsg" {
+resource "azurerm_network_security_group" "mycicdnsg" {
     name                = "myNetworkSecurityGroup"
     location            = "West Europe"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
+    resource_group_name = azurerm_resource_group.cicdgroup.name
 
     security_rule {
         name                       = "SSH"
@@ -76,73 +76,73 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "CICD Infrastructure"
     }
 }
 
 # Create network interface
-resource "azurerm_network_interface" "myterraformnic" {
+resource "azurerm_network_interface" "mycicdnic" {
     name                      = "myNIC"
     location                  = "West Europe"
-    resource_group_name       = azurerm_resource_group.myterraformgroup.name
+    resource_group_name       = azurerm_resource_group.cicdgroup.name
 
     ip_configuration {
         name                          = "myNicConfiguration"
-        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        subnet_id                     = azurerm_subnet.mycicdsubnet.id
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+        public_ip_address_id          = azurerm_public_ip.mycicdpublicip.id
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "CICD Infrastructure"
     }
 }
 
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "example" {
-    network_interface_id      = azurerm_network_interface.myterraformnic.id
-    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+    network_interface_id      = azurerm_network_interface.mycicdnic.id
+    network_security_group_id = azurerm_network_security_group.mycicdnsg.id
 }
 
 # Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group is defined
-        resource_group = azurerm_resource_group.myterraformgroup.name
-    }
+# resource "random_id" "randomId" {
+#     keepers = {
+#         # Generate a new ID only when a new resource group is defined
+#         resource_group = azurerm_resource_group.cicdgroup.name
+#     }
 
-    byte_length = 8
-}
+#     byte_length = 8
+# }
 
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "mystorageaccount" {
     name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
+    resource_group_name         = azurerm_resource_group.cicdgroup.name
     location                    = "West Europe"
     account_tier                = "Standard"
     account_replication_type    = "LRS"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "CICD Infrastructure"
     }
 }
 
 # Create (and display) an SSH key
-resource "tls_private_key" "example_ssh" {
+resource "tls_private_key" "cicd_ssh" {
   algorithm = "RSA"
   rsa_bits = 4096
 }
 output "tls_private_key" { 
-  value = tls_private_key.example_ssh.private_key_pem 
+  value = tls_private_key.cicd_ssh.private_key_pem 
   sensitive = true 
   }
 
 # Create virtual machine
-resource "azurerm_linux_virtual_machine" "myterraformvm" {
+resource "azurerm_linux_virtual_machine" "mycicdvm" {
     name                  = "myVM"
     location              = "West Europe"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    resource_group_name   = azurerm_resource_group.cicdgroup.name
+    network_interface_ids = [azurerm_network_interface.mycicdnic.id]
     size                  = "Standard_DS1_v2"
 
     os_disk {
@@ -164,7 +164,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
 
     admin_ssh_key {
         username       = "azureuser"
-        public_key     = tls_private_key.example_ssh.public_key_openssh
+        public_key     = tls_private_key.cicd_ssh.public_key_openssh
     }
 
     boot_diagnostics {
@@ -172,6 +172,6 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "CICD Infrastructure"
     }
 }
