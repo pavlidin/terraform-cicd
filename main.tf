4675204d3723febe4d3967e89ac5497a59ee9911
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-05-29 16:43:57 +0300

    refactored init 2

--- main.tf
+++ main.tf
@@ -58,6 +58,32 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_public_ip" "cicd" {
+  name                = "cicd-publicIP"
+  location            = azurerm_resource_group.cicd.location
+  resource_group_name = azurerm_resource_group.cicd.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+# Network Security Groups control the flow of network traffic in and out of your VM.
+resource "azurerm_network_security_group" "cicd" {
+  name                = "cicdSG"
+  location            = azurerm_resource_group.cicd.location
+  resource_group_name = azurerm_resource_group.cicd.name
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = 22
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_network_interface" "cicd" {
   name                = "cicd-nic"
   location            = azurerm_resource_group.cicd.location
