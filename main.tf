69889f9a150e52485c03bcafaa70278991257a88
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-05-29 15:51:00 +0300

    add cicd init vm

--- main.tf
+++ main.tf
@@ -22,16 +22,79 @@ provider "azurerm" {
   tenant_id = var.tenant_id
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+
+#Application VMs
+resource "azurerm_resource_group" "application" {
+  name     = "Application Resources"
   location = "West Europe"
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_virtual_network" "application-network" {
+  name                = "Application Network"
+  resource_group_name = azurerm_resource_group.application.name
+  location            = azurerm_resource_group.application.location
   address_space       = ["10.0.0.0/16"]
+}
+
+
+#CI/CD VM
+
+resource "azurerm_resource_group" "cicd" {
+  name     = "cicd-resources"
+  location = "West Europe"
+}
+
+# resource "azurerm_virtual_network" "cicdnetwork" {
+#   name                = "CI/CD Network"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.cicdnetwork.location
+#   resource_group_name = azurerm_resource_group.cicdnetwork.name
+# }
+
+# resource "azurerm_subnet" "example" {
+#   name                 = "internal"
+#   resource_group_name  = azurerm_resource_group.example.name
+#   virtual_network_name = azurerm_virtual_network.example.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
+
+resource "azurerm_network_interface" "cicd" {
+  name                = "cicd-nic"
+  location            = azurerm_resource_group.cicd.location
+  resource_group_name = azurerm_resource_group.cicd.name
+
+  ip_configuration {
+    name                          = "internal"
+    # subnet_id                     = azurerm_subnet.cicd.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "cicd" {
+  name                = "cicd-machine"
+  resource_group_name = azurerm_resource_group.cicd.name
+  location            = azurerm_resource_group.cicd.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.cicd.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
 }
