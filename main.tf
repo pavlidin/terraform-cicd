3d0b4dbbafac0b4934a70a4180a8f816d3443f7e
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-05-29 14:42:43 +0300

    fix names

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_resource_group" "example-2" {
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example-2.name
+  location            = azurerm_resource_group.example-2.location
   address_space       = ["10.0.0.0/16"]
 }
