9d18c0ef679c3909e2483e9e846ba9acc5e9a8dd
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-06-01 17:36:05 +0300

    add remote-exec provisioner 2

--- main.tf
+++ main.tf
@@ -194,7 +194,9 @@ resource "azurerm_linux_virtual_machine" "cicdvm" {
       "yum install -y yum-utils",
       "yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
       "yum install -y docker-ce docker-ce-cli containerd.io",
-      "systemctl start docker"
+      "systemctl start docker",
+      "systemctl enable docker.service",
+      "systemctl enable containerd.service"
     ]
   }
 }
