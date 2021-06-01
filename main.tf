f5d985b7d8d6fb38213b69d9d57644048a78707b
Nikos Pavlidis Skarlatos <nikospavlidismail@gmail.com>
2021-06-01 18:48:25 +0300

    add remote-exec provisioner 14

--- main.tf
+++ main.tf
@@ -177,7 +177,7 @@ resource "azurerm_linux_virtual_machine" "cicdvm" {
       user = "azureuser"
       private_key = var.private_key
       timeout = "2m"
-      host = azurerm_public_ip.cicdpublicip.ip_address
+      host = data.azurerm_public_ip.cicd.ip_address
     }
     inline = [
       "sudo su",
